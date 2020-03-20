f5e503e3a4c35af4cbbc4c12ef33103aa7e3b717
Henry <henrybsms@outlook.com>
2020-03-20 14:37:51 -0400

    worked on index added folder

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { Reducer } from "./reducers/Reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+
+const store = createStore( applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root')
+);
