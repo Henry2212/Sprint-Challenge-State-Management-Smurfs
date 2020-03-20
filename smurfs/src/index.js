07ef884fc0c6304cb7b3e9243a53f6a849f4c1ca
Henry <henrybsms@outlook.com>
2020-03-20 15:33:42 -0400

    MVP

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,17 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
-import thunk from "redux-thunk";
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import { Reducer } from "./reducers/Reducers";
 
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import reducer from './reducers'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 
-
-const store = createStore(Reducer,applyMiddleware(thunk));
+const composeEnhancers = window._|| compose;
+const store = createStore(
+    reducer,
+    composeEnhancers(applyMiddleware(thunk, logger))
+    ) 
 
 ReactDOM.render(
-    <Provider store={store}>
+    <Provider store ={store}>
         <App />
-    </Provider>, document.getElementById('root')
-);
+    </Provider>, 
+document.getElementById('root'));
